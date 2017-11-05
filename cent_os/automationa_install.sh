c9c63747d2669ccc810521c2255ff69ad09c76fd
Giorgi Keratishvili <13200109@ibsu.edu.ge>
2017-11-05 19:59:17 +0400

    Update automationa_install.sh

--- cent_os/automationa_install.sh
+++ cent_os/automationa_install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+function update_upgrade{
+yum -y update  > /dev/null 2>&1
+yum -y upgrade > /dev/null 2>&1
+}
+
+
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # After vanila installation we must make update
-yum -y update
-yum -y upgrade
+update_upgraderade
+
 
 # Cleaning  Centos_repo list
-cd $CURRENT_DIR 
-cat CentOS-Base.repo > /etc/yum.repos.d/CentOS-Base.repo  
+cd $CURRENT_DIR
+cat CentOS-Base.repo > /etc/yum.repos.d/CentOS-Base.repo
 yum -y update
 
 # Adding extended package repo
 yum -y install http://mirror.itdc.ge/epel/7/`arch`/e/epel-release-7-5.noarch.rpm
 
 
-# Installing packages 
-cat packages.txt | xargs yum -y install 2> .error.log
-
-# Update
-yum -y update
-yum -y upgrade
+# Installing packages
+cat packages.txt | xargs yum -y -e1 install
 
